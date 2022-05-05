085aad2ca30a3b47763759ec6f3e8bec42003cf8
Sathyannsp <sathyan02021996@gmail.com>
2022-05-05 21:42:45 +0530

    initial commit

---
+++ src/components/about/About.js
@@ -0,0 +1,43 @@
+import "./About.css";
+import Award from "../../img/award.png";
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be readable content
+        </p>
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <div className="a-award">
+          <img src={Award} alt="" className="a-award-img" />
+          <div className="a-award-texts">
+            <h4 className="a-awards-title">International Design</h4>
+            <p className="a-award-desc">
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,94 @@
+import "./Contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useContext, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { ThemeContext } from "../../Context";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_04vzkmp",
+        "template_um6o87e",
+        formRef.current,
+        "Zmeqi_4HNHIXk-PRe"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +91 12345 67890
+            </div>
+            <div className="c-info-item">
+              <img src={Email} alt="" className="c-icon" />
+              sathyan@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img src={Address} alt="" className="c-icon" />
+              25, baker street
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What's your story</b> Get in touch. Always available for
+            freelancing if the right project comes along
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Subject"
+              name="user_subject"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{ backgroundColor: darkMode && "#333" }}
+              rows="5"
+              placeholder="Message"
+              name="message"
+            />
+            <button>Submit</button>
+            {done && "Thank You..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
